3152e985ea99981e928579e58939a3060fa5f4d3
Omar Bouallou <omar.bouallou@external.grtgaz.com>
2021-04-13 12:53:33 +0200

    Resolve test

--- domain/src/main/java/mixter/domain/core/subscription/Subscription.java
+++ domain/src/main/java/mixter/domain/core/subscription/Subscription.java
@@ -1,13 +1,44 @@
 package mixter.domain.core.subscription;
 
 import mixter.doc.Aggregate;
+import mixter.doc.Projection;
+import mixter.domain.DecisionProjectionBase;
+import mixter.domain.Event;
 import mixter.domain.EventPublisher;
 import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
 import mixter.domain.identity.UserId;
+
+import java.util.List;
 
 @Aggregate
 public class Subscription {
+
+    private final SubscriptionProjection projection;
+
+    public Subscription(List<Event> history) {
+        this.projection = new SubscriptionProjection(history);
+    }
+
     public static void follow(UserId follower, UserId followee, EventPublisher eventPublisher) {
         eventPublisher.publish(new UserFollowed(new SubscriptionId(follower, followee)));
+    }
+
+    public void unfollow(EventPublisher eventPublisher) {
+        eventPublisher.publish(new UserUnfollowed(projection.id));
+    }
+
+    @Projection
+    private class SubscriptionProjection extends DecisionProjectionBase {
+        private SubscriptionId id;
+
+        public SubscriptionProjection(List<Event> history) {
+            super.register(UserFollowed.class, this::apply);
+            history.forEach(this::apply);
+        }
+
+        private void apply(UserFollowed event) {
+            this.id = event.getSubscriptionId();
+        }
     }
 }
