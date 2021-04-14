d1b34a103b017e50c4ae29c0fc29ffef39228d28
Omar Bouallou <omar.bouallou@external.grtgaz.com>
2021-04-14 13:05:47 +0200

    Resolve test

--- domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
+++ domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
@@ -4,6 +4,7 @@ import mixter.doc.Handler;
 import mixter.domain.core.subscription.FollowerRepository;
 import mixter.domain.core.subscription.SubscriptionId;
 import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
 
 @Handler
 public class UpdateFollowers {
@@ -14,5 +15,10 @@ public class UpdateFollowers {
     public void apply(UserFollowed userFollowed) {
         SubscriptionId subscriptionId = userFollowed.getSubscriptionId();
         repository.saveFollower(subscriptionId.getFollowee(), subscriptionId.getFollower());
+    }
+
+    public void apply(UserUnfollowed userUnfollowed) {
+        SubscriptionId subscriptionId = userUnfollowed.getSubscriptionId();
+        repository.removeFollower(subscriptionId.getFollowee(), subscriptionId.getFollower());
     }
 }
